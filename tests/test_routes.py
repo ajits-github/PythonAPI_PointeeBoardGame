ec22b37d46c1f9783c041d6cd478bd206888e470
Ajit <ajit110920@gmail.com>
2023-10-27 23:02:42 +0200

    invalid grounds

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -11,4 +11,8 @@ class TestAPIRoutes(unittest.TestCase):
         data = response.get_json()
         self.assertIn("coupon_values", data)
         self.assertIn("highest_value_coupons", data)
-
+    
+    def test_play_game_invalid_rounds(self):
+        response = self.client.get('/play/10')
+        self.assertEqual(response.status_code, 200, "Inside INVALID rounds: Expected status code 200 but got {}".format(response.status_code))
+        self.assertEqual(response.data, b"Valid redemption rounds are 25, 50, or 100.")
