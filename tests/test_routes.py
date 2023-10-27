3d08c54aec5b18fec0741b7b4fe3fb0f2ba78dfc
Ajit <ajit110920@gmail.com>
2023-10-27 23:02:00 +0200

    valid grounds

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,4 +5,10 @@ class TestAPIRoutes(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
-    
+    def test_play_game_valid_rounds(self):
+        response = self.client.open(method='GET', path='/play/25')
+        self.assertEqual(response.status_code, 200, "Inside VALID rounds: Expected status code 200 but got {}".format(response.status_code))
+        data = response.get_json()
+        self.assertIn("coupon_values", data)
+        self.assertIn("highest_value_coupons", data)
+
